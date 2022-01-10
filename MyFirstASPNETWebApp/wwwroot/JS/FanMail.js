a558d3b4d7d7c56907f6ccde61c79dfb0eecb961
yejinchoi115 <yjjjang115@gmail.com>
2022-01-09 23:26:20 -0700

    Initial check-in of module MyFirstASPNETWebApp

---
+++ MyFirstASPNETWebApp/wwwroot/JS/FanMail.js
@@ -0,0 +1,15 @@
+﻿//document.getElementById("btnSend").addEventListener("click", function () {
+//    alert( "From: " + document.getElementById("txtFrom").value +
+//      ", Subject: " + document.getElementById("txtSubject").value +
+//        ", Message: " + document.getElementById("txtMessage").value );
+//})
+// without jquery
+
+$("#btnSend").click(function () {
+    alert("From: " + $("#txtFrom").val() + ", Subject: " +
+        $("#txtSubject").val() + ", Message: " + 
+        $("#txtMessage").val());
+
+    $("#picGanderson").fadeOut("slow");
+}) //jquery way
+
